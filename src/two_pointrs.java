2e3dd156f3ff4a2d99381e6daca818cb6f9fb50a
Parin Chavda <parinchavda.17@gmail.com>
2025-10-30 12:33:49 +0530

    add all

---
+++ src/two_pointrs.java
@@ -0,0 +1,40 @@
+public class two_pointrs {
+     static void printarr(int[] arr)
+    {
+        for (int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void sortetwopointers(int[] arr)
+    {
+        int count = 0 ;
+        for (int i=0;i<arr.length;i++)
+        {
+            if (arr[i] == 0)
+            {
+                count++;
+            }
+        }
+        for (int i=0;i<arr.length;i++)
+        {
+            if ( i < count)
+            {
+                arr[i] = 0 ;
+            }
+            else
+            {
+                arr[i] = 1 ;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[]{0,1,0,1,0,1,0,1,0,1,0,0,0,1};
+        printarr(arr);
+        sortetwopointers(arr);
+        System.out.println();
+        printarr(arr);
+
+    }
+}
